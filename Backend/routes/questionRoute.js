f6dc3b918a9efafa68b418f762dc4be660e4722a
dumplingpleats <dumplingpleats@gmail.com>
2024-12-11 23:41:23 +0500

    test

--- Backend/routes/questionRoute.js
+++ Backend/routes/questionRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { 
     getQuestionsByQuizId 
 
-} = require('../controllers/questionController');
+} = require('../Controllers/questionController');
 
 // Route to GET questions for a specific quiz
 router.get('/quiz/:quizId/questions', getQuestionsByQuizId);
